09450991da7db2344fbf9c0e8d0f0d07d10cccf9
DJ Kullas <dkoolj5@gmail.com>
2021-07-15 22:04:03 -0400

    started authentication

--- server.js
+++ server.js
@@ -4,8 +4,60 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const { Client } = require('pg');
+var session = require('express-session');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 
-console.log(process.env.DATABASE_URL);
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+
+// config express-session
+var sess = {
+  secret: 'jgioudsfhngupfdnbsgiusdnignjdsioahj43589fb4u',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  app.set('trust proxy', 1);
+}
+
+app.use(session(sess));
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
